1a2c30f423a44a9ebd9a58ab5b59a501ccb5750e
hongxuan <hongxuan@n0h4ts.com>
2023-10-17 17:52:38 +0800

    update

---
+++ _next/static/iCyu4teXpKDFprEXizDnb/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/131-072cd3d2e02a16e5.js","static/chunks/pages/index-da6cf816cf82868f.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
